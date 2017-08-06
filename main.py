78d0c8e9cb2d5db8f4f71d6bf14956fe8085ec52
quanvuong <quan.vuong@nyu.edu>
2017-08-06 12:47:04 +0800

    set constants to run analysis for Chinese characters

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ chinese_human_scores = [
     i for i in range(15)
 ]
 
-human_scores = chipman_human_scores
-# human_scores = chinese_human_scores
+# human_scores = chipman_human_scores
+human_scores = chinese_human_scores
 
 def sort_picture(picture_names):
     change = False
@@ -136,13 +136,12 @@ def get_image_names(folder_name):
 if __name__ == '__main__':
 
     # The folder in which the images are in
-    folder_name = 'Chipman patterns'
-    # Excel file name
-    excel_file_name = 'Chipman Image Analysis.xlsx'
+    # folder_name = 'Chipman patterns'
+    # excel_file_name = 'Chipman Image Analysis.xlsx'
 
     # Chinese data
-    # folder_name = 'Chinese Characters/Chinese Compressed'
-    # excel_file_name = 'Chinese Compressed Character Image Analysis.xlsx'
+    folder_name = 'Chinese Characters/Chinese Compressed'
+    excel_file_name = 'Chinese Compressed Character Image Analysis.xlsx'
 
     # image_score contains the scores of all images
     image_score = {}
@@ -207,23 +206,23 @@ if __name__ == '__main__':
 
     print('Saved analysis to', excel_file_name)
 
-    rb1 = xlrd.open_workbook(excel_file_name)
-    rb2 = xlrd.open_workbook('Unoptimized_Chipman Image Analysis.xlsx')
-
-    sheet1 = rb1.sheet_by_index(0)
-    sheet2 = rb2.sheet_by_index(0)
-
-    for rownum in range(max(sheet1.nrows, sheet2.nrows)):
-        if rownum < sheet1.nrows:
-            row_rb1 = sheet1.row_values(rownum)
-            row_rb2 = sheet2.row_values(rownum)
-
-            for colnum, (c1, c2) in enumerate(izip_longest(row_rb1, row_rb2)):
-                if c1 != c2:
-                    print "Row {} Col {} - {} != {}".format(rownum+1, colnum+1, c1, c2)
-        else:
-            print "Row {} missing".format(rownum+1)
-    print('New Results equal to old unoptimized code results')
+    # rb1 = xlrd.open_workbook(excel_file_name)
+    # rb2 = xlrd.open_workbook('Unoptimized_Chipman Image Analysis.xlsx')
+    #
+    # sheet1 = rb1.sheet_by_index(0)
+    # sheet2 = rb2.sheet_by_index(0)
+    #
+    # for rownum in range(max(sheet1.nrows, sheet2.nrows)):
+    #     if rownum < sheet1.nrows:
+    #         row_rb1 = sheet1.row_values(rownum)
+    #         row_rb2 = sheet2.row_values(rownum)
+    #
+    #         for colnum, (c1, c2) in enumerate(izip_longest(row_rb1, row_rb2)):
+    #             if c1 != c2:
+    #                 print "Row {} Col {} - {} != {}".format(rownum+1, colnum+1, c1, c2)
+    #     else:
+    #         print "Row {} missing".format(rownum+1)
+    # print('New Results equal to old unoptimized code results')
 
 
 # The commentted part below is for debugging
